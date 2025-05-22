2d89105491f9e76be9a6248f891d20142b72fa03
avishkarangana <167955408+avishkarangana@users.noreply.github.com>
2025-05-22 23:40:34 +0530

    Update main.c
    Code function to Full message encryption / decryption

--- main.c
+++ main.c
@@ -27,3 +27,17 @@ char shiftDecryptChar(char ch, int key) {
     int index = (ch - 'A' - key + SIZE) % SIZE;
     return alphabetQueue[index];
 }
+
+void encryptMessage(const char *input, int key, char *output) {
+    for (int i = 0; input[i] != '\0'; i++) {
+        output[i] = shiftEncryptChar(input[i], key);
+    }
+    output[strlen(input)] = '\0';
+
+void decryptMessage(const char *input, int key, char *output) {
+    for (int i = 0; input[i] != '\0'; i++) {
+        output[i] = shiftDecryptChar(input[i], key);
+    }
+    output[strlen(input)] = '\0';
+
+
